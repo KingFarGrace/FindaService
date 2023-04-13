3e5582255eb44b77dc3a7e9b7102de85fcb5c900
KingFarGrace <kingfargrace@163.com>
2023-04-13 21:23:37 +0100

    Interface register: need test. - New dir utils: store util modules 	- respUtil: util functions for responses 	- validateUtil: util functions for validation - code_msg.js: store code-msg pairs for responses - Finish register interface, but need more test.

--- backend/router/user.js
+++ backend/router/user.js
@@ -1,11 +1,7 @@
 const express = require('express')
-// const expressJoi = require('@escook/express-joi')
 const router = express.Router()
 const userHandler = require('../router_handler/user')
-// const { reg_login_schema } = require('../schema/user')
 
-// router.post('/register', expressJoi(reg_login_schema), userHandler.register)
-// router.post('/login', expressJoi(reg_login_schema), userHandler.login)
 router.post('/register', userHandler.register)
 router.post('/login',userHandler.login)
 
