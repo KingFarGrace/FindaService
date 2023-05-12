2bf029ace7dd5be2aec630faa5c49e39354bf993
Zhang <1821957132@qq.com>
2023-05-12 21:29:59 +0100

    改了下退出登录的bug

--- frontend/src/pages/Admin/Admin.js
+++ frontend/src/pages/Admin/Admin.js
@@ -39,9 +39,10 @@ const App = () => {
   const user = storageUtils.getUser();
   //若内存中存了用户名，则已登录，否则跳转至登录界面
   // 前后跑通再取消注释是
-  // if (!user.username) {
-  //   return <Redirect to='/login' />
-  // }
+  //打个异或
+  if (!((!user.username)^(!user.email))) {
+    return <Redirect to='/login' />
+  }
 
   return (
 
